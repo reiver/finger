a62df6023b386de7ec3810f0cf90edc6860e5e63
Charles Iliya Krempeaux <iamreiver@gmail.com>
2022-12-01 03:32:37 +0900

    initial commits

--- srv/failure/publish.go
+++ srv/failure/publish.go
@@ -5,6 +5,22 @@ import (
 	"os"
 )
 
+// Publish is used to output error messages that are sent just before the program exits (due to an error).
+//
+// For example:
+//
+//	package main
+//	
+//	import failure "github.com/reiver/go-finger/srv/failure"
+//	
+//	// ...
+//	
+//	err := something()
+//	if nil != err {
+//		failure.Publish("we have a problem: %s", err)
+//		os.Exit(1)
+//		return
+//	}
 func Publish(format string, a ...interface{}) {
 	format = fmt.Sprintf("%s\n", format)
 
